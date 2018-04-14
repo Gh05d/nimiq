efee7ea9679294765362d1bd653f049ca85e470a
Pascal Clanget <pascal.clanget@googlemail.com>
2018-04-14 20:04:38 +0200

    Create start-dumb-miner.sh

---
+++ start-dumb-miner.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo 'Please enter your wallet address: '
+read WALLET
+
+echo 'Please enter the number of threads you want to use: '
+reat THREADS
+cd core/clients/nodejs
+
+echo "The miner runs with $THREADS threads."
+
+env UV_THREADPOOL_SIZE=128 node index.js --dumb --statistics \
+ --miner=$THREADS --network=main \
+ --wallet-address \"$WALLET"\
